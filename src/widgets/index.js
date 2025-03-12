77ab4731e70f92da1357bd01e7029e0827438f29
Amange1di <amangeldi2702006@gmail.com>
2025-03-12 16:57:07 +0300

    kjkjljljjkjjk

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -16,14 +16,13 @@ export { AboutTexts } from "./aboutSection/aboutTexts/AboutTexts"
 export { AboutNavigate } from "./aboutSection/aboutNavigate/AboutNavigate"
 
 
-export  {Banner} from "./homeSection/banner/Banner"
-export {Events} from "./homeSection/events/Events"
-export {News} from "./homeSection/news/News"
+export { Banner } from "./homeSection/banner/Banner"
+export { Events } from "./homeSection/events/Events"
+export { News } from "./homeSection/news/News"
 export { Jurnal } from './homeSection/sliderAcademy/Jurnal';
 export { JurnalComp } from "./homeSection/sliderAcademy/JurnalComp"
 export { SliderHome } from "./homeSection/sliderHome/SliderHome"
-export {JurnalVuzSl} from "./homeSection/sliderPartner/JurnalVuzSl"
-export { LinkSlider } from './homeSection/sliderLinks/LinkSlider';
+export { JurnalVuzSl } from "./homeSection/sliderPartner/JurnalVuzSl"
 
 export { JurnalVuz } from "./homeSection/sliderPartner/JurnalVuz"
 export { Education } from "./homeSection/stepeni/Education"
@@ -34,9 +33,10 @@ export { GalleryBanner } from "./gallerySection/galleryBanner/GalleryBanner"
 export { GalleryImg } from "./gallerySection/galleryImg/GalleryImg"
 
 
-export {Academy} from "./scienceSection/academiccouncil/Academy"
-export {ScienceCenter} from "./scienceSection/sciencecenter/ScienceCenter"
-export {Scientific} from "./scienceSection/scientificjournals/Scientific"
+export { Academy } from "./scienceSection/academiccouncil/Academy"
+export { ScienceCenter } from "./scienceSection/sciencecenter/ScienceCenter"
+export { Scientific } from "./scienceSection/scientificjournals/Scientific"
 
- export {Cpo} from "./educationSection/cpo/Cpo"
-export {Student} from "./studentsSection/Student"
+export { Cpo } from "./educationSection/cpo/Cpo"
+export { Student } from "./studentsSection/Student"
+export {LinkSlider} from "./homeSection/sliderLinks/LinkSlider"
